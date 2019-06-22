61875abff36687448db26ae114b78afdd6cb6192
kubill <meiweijia@vip.qq.com>
2019-06-22 17:35:07 +0800

    客房维修增加维修事项字段

--- app/Http/Controllers/RepairController.php
+++ app/Http/Controllers/RepairController.php
@@ -14,7 +14,7 @@ class RepairController extends ApiController
 
     public function store(Request $request)
     {
-        $result = $request->user()->repairs->create($request->only([
+        $result = $request->user()->repairs()->create($request->only([
             'house_id',
             'name',//签约人姓名
             'repair_date',//维修日期
